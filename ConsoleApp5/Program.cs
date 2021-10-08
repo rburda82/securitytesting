cede72f97bebc53fd7784edb9bdb3239ed19c49b
roman.burda <roman.burda@justanswer.com>
2021-10-08 12:59:21 +0300

    test commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = JsonConvert.DeserializeObject<object>(args[1], new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            });
+
+
+            Console.WriteLine(value.ToString());
+        }
+    }
+}
